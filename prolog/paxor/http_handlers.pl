1bb4c53a594484f1169ac63800b1eac3dc145a4b
Roy Ratcliffe <royratcliffe@me.com>
2021-04-06 20:24:32 +0000

    Get and set Paxos key-data pairs

--- prolog/paxor/http_handlers.pl
+++ prolog/paxor/http_handlers.pl
@@ -1,10 +1,25 @@
 :- module(paxor_http_handlers, []).
 :- autoload(library(http/http_dispatch), [http_handler/3]).
+:- autoload(library(http/http_client), [http_read_data/3]).
 :- autoload(library(http/http_json), [reply_json/1]).
-:- autoload(library(paxos), [paxos_property/1]).
+:- autoload(library(paxos), [paxos_property/1, paxos_get/2, paxos_set/2]).
 
 :- http_handler(root(paxor/properties), properties, []).
+:- http_handler(root(paxor/Key), key(Method, Key),
+                [ method(Method),
+                  methods([get, post])
+                ]).
 
 properties(_Request) :-
     findall(A=B, (paxos_property(C), C =.. [A, B]), D),
     reply_json(json(D)).
+
+key(get, Key, _Request) :-
+    (   paxos_get(Key, Data)
+    ->  JSON = [data=Data]
+    ;   JSON = []
+    ),
+    reply_json(json(JSON)).
+key(post, Key, Request) :-
+    http_read_data(Request, Data, []),
+    paxos_set(Key, Data).
