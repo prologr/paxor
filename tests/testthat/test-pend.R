06f9353c18d5731acbf571859f8cf7ffd1b66394
Roy Ratcliffe <royratcliffe@me.com>
2021-04-06 19:00:09 +0000

    Tests

---
+++ tests/testthat/test-pend.R
@@ -0,0 +1,11 @@
+test_that("pend get works", {
+  expect_equal(paxor::pend.get(), NULL)
+  expect_equal(paxor::pend.get("xyz"), c("xyz"))
+  expect_equal(paxor::pend.get("abc"), c("xyz", "abc"))
+})
+
+test_that("pend set works", {
+  expect_equal(paxor::pend.set(), list())
+  expect_equal(paxor::pend.set(a = 1, b = 2, c = 3), list(a = 1, b = 2, c = 3))
+  expect_equal(paxor::pend.set(a = 123), list(a = 123, b = 2, c = 3))
+})
