0e9945914cfb88249ee717c76266aa1ba3977340
Roy Ratcliffe <royratcliffe@me.com>
2021-04-13 20:44:38 +0000

    r_eval:eval/2 takes string

--- prolog/r/eval.pl
+++ prolog/r/eval.pl
@@ -2,11 +2,9 @@
           [ eval/2
           ]).
 :- use_module(library(strings)).
-:- use_module(library(readutil)).
-:- use_module(library(r/r_serve)).
+:- use_module(library(r/r_call)).
 
 eval(A, E) :-
-    read_file_to_string(A, B, []),
-    split_string(B, "\n", "\r", C),
+    split_string(A, "\n", "\r", C),
     string_lines(D, C),
     r_eval_ex($, D, E).
