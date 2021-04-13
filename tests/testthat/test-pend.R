e40c5f47d68247e1987416ed3c2d5679ef50dd56
Roy Ratcliffe <royratcliffe@me.com>
2021-04-13 06:08:48 +0000

    Revert "Lower-case get, set for pend"
    This reverts commit 18920555d630a8b3b449beb48b3ea11fb50a7518.

--- tests/testthat/test-pend.R
+++ tests/testthat/test-pend.R
@@ -1,11 +1,11 @@
 test_that("pend get works", {
-  expect_equal(paxor::get(), c())
-  expect_equal(paxor::get("xyz"), c("xyz"))
-  expect_equal(paxor::get("abc"), c("xyz", "abc"))
+  expect_equal(paxor::pend.get(), c())
+  expect_equal(paxor::pend.get("xyz"), c("xyz"))
+  expect_equal(paxor::pend.get("abc"), c("xyz", "abc"))
 })
 
 test_that("pend set works", {
-  expect_equal(paxor::set(), list())
-  expect_equal(paxor::set(a = 1, b = 2, c = 3), list(a = 1, b = 2, c = 3))
-  expect_equal(paxor::set(a = 123), list(a = 123, b = 2, c = 3))
+  expect_equal(paxor::pend.set(), list())
+  expect_equal(paxor::pend.set(a = 1, b = 2, c = 3), list(a = 1, b = 2, c = 3))
+  expect_equal(paxor::pend.set(a = 123), list(a = 123, b = 2, c = 3))
 })
