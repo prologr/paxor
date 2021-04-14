74a96fe4e894b62ccd6d7d525dfa802c9216c179
Roy Ratcliffe <royratcliffe@me.com>
2021-04-14 05:56:58 +0000

    Better variable names

--- prolog/r/eval.pl
+++ prolog/r/eval.pl
@@ -5,7 +5,7 @@
 :- use_module(library(r/r_serve)).
 :- use_module(library(r/r_call), []).
 
-eval_r(A, E) :-
-    split_string(A, "\n", "\r", C),
-    string_lines(D, C),
-    r_eval_ex($, D, E).
+eval_r(R, Term) :-
+    split_string(R, "\n", "\r", Lines),
+    string_lines(R_, Lines),
+    r_eval_ex($, R_, Term).
